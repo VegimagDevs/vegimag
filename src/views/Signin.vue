4ac5086dab0d24f6b9ec5e8ad6b5a87d8d3d9cde
Mathilde Grapin <mathilde@koicactus.net>
2020-05-06 21:05:36 +0200

    Feature signin

---
+++ src/views/Signin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <form>
+      <label for="email">Adresse email :</label>
+      <input type="text" name="email" placeholder="Email"><br>
+
+      <label for="password">Mot de passe :</label>
+      <input type="password" name="password" placeholder="Mot de passe"><br>
+      <button>Se connecter</button>
+    </form>
+    <p>Mot de passe oublié ?</p>
+    <p>Pas de compte ?</p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class Signin extends Vue {
+
+}
+</script>
