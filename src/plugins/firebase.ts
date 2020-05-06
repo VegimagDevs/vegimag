d6009ba7e73dc313e1731bb32e4d0f2b658eee26
Johyn Papin <johynpapin@koicactus.net>
2020-05-06 20:00:51 +0200

    Initial commit

---
+++ src/plugins/firebase.ts
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAPbifSfwU5231lmVog2jgsap_obuV8fhg',
+  authDomain: 'vegimag-b0a13.firebaseapp.com',
+  databaseURL: 'https://vegimag-b0a13.firebaseio.com',
+  projectId: 'vegimag-b0a13',
+  storageBucket: 'vegimag-b0a13.appspot.com',
+  messagingSenderId: '466157461876',
+  appId: '1:466157461876:web:438471919584fb0ef24c21'
+}
+
+firebase.initializeApp(firebaseConfig)
