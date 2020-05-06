8652a7b1d93027bd7180d0fac6165672c20c8956
Johyn Papin <johynpapin@koicactus.net>
2020-05-06 21:36:25 +0200

    fixing problems

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,8 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
-<<<<<<< HEAD
-import SignUp from '../views/SignUp.vue'
-=======
 import SignIn from '../views/SignIn.vue'
-import Signup from '../views/Signup.vue'
->>>>>>> d0a65cf462ddf4e460d8ba32f439478bda36cd47
+import SignUp from '../views/SignUp.vue'
 
 Vue.use(VueRouter)
 
@@ -17,22 +13,14 @@ const routes: Array<RouteConfig> = [
     component: Home
   },
   {
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUp
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/signin',
     name: 'SignIn',
     component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
   }
 ]
 
