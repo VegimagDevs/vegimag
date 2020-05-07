d41e71b25cd6f4e0799c5f58dfde73309aa1610b
Mathilde Grapin <mathilde@koicactus.net>
2020-05-07 20:36:47 +0200

    Customize home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1 class="text-center">Vegimag</h1>
+    <div class="flex mb-4">
+      <div class="w-full bg-gray-500 h-12">
+        <h1 class="">Vegimag</h1>
+      </div>
+    </div>
+
     <h2 class="text-center">Panier de la semaine</h2>
     <div class="flex flex-row bg-gray-200">
       <button class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2" v-for="ingredient in basket" :key="ingredient.name">{{ ingredient.name }}</button>
